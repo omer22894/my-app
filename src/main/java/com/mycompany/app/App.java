2394ae34d1d7772a56da6b3eecf9e39daec029af
omer22894 <110353934+omer22894@users.noreply.github.com>
2022-08-18 19:47:40 -0400

    new lines entered

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!...Changes into source code for testing for next version v2.0" );
+        System.out.println( "Hello World!...Changes into source code for testing for next version v3.0" );
     }
 }
