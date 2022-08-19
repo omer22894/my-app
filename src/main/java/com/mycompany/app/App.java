5b5533113e29943a2f4a22ac8f8cb4c4bb623869
omer22894 <110353934+omer22894@users.noreply.github.com>
2022-08-18 20:59:11 -0400

    updated version7.3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!...Changes into source code for testing for next version v5.2 by aws and devops engg md omer faruq" );
+        System.out.println( "Hello World!...Changes into source code for testing for next version v7.2 by aws and devops engg md omer faruq" );
     }
 }
